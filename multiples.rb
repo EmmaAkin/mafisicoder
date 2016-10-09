fae48fe208ab4c6f7d9c3c96649b46a7d12167e2
emmaAkin <akinpense2@gmail.com>
2016-10-09 20:59:02 +0200

    Week 2 challenge given by Emma

---
+++ multiples.rb
@@ -0,0 +1,13 @@
+i = 1
+while i<=50
+    if i%3 ==0 and i%5==0
+        puts "MEST_MINC"
+    elsif i%3 ==0
+        puts "MEST"
+    elsif i%5 == 0
+        puts "MINC"
+    else
+        puts i
+    end
+        i +=1
+end
